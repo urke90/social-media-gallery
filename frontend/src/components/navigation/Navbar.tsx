727959f6a7988962fd43ec0d2726a7666413ff50
urke90 <urosbijelic90@gmail.com>
2024-01-31 17:23:13 +0100

    Navbar component created with search and user image with link to create-pin page

--- frontend/src/components/navigation/Navbar.tsx
+++ frontend/src/components/navigation/Navbar.tsx
@@ -1,12 +1,49 @@
+import { Link, useNavigate } from 'react-router-dom';
+// react icons
+import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+// types
+import { type ISanityUser } from '@/models';
+
 // ----------------------------------------------------------------
 
 type NavbarProps = {
   searchQuery: string;
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  user: ISanityUser | null;
 };
 
-const Navbar: React.FC<NavbarProps> = (props) => {
-  return <div>Navbar</div>;
+const Navbar: React.FC<NavbarProps> = ({ searchQuery, setSearchQuery, user }) => {
+  console.log('user', user);
+  const navigate = useNavigate();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex gap-2 md:gap-2 w-full mt-5 pb-7">
+      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+        <IoMdSearch fontSize={21} className="ml-1" />
+        <input
+          type="text"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search"
+          value={searchQuery}
+          onFocus={() => navigate('/search')}
+          className="p-2 w-full bg-white outline-none"
+        />
+      </div>
+      <div className="flex gap-3">
+        <Link to={`user-profile/${user._id}`} className="hidden md:block">
+          <img src={user?.image} alt={user.userName} className="w-14 h-12 rounded-lg" />
+        </Link>
+        <Link
+          to="create-pin"
+          className="bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
+        >
+          <IoMdAdd />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/hooks/common/use-redirect.ts
+++ frontend/src/hooks/common/use-redirect.ts
@@ -1,13 +1,12 @@
 // react router
-import { useNavigate, NavigateOptions } from "react-router-dom";
+import { useNavigate, NavigateOptions } from 'react-router-dom';
 
 // ----------------------------------------------------------------
 
 export const useRedirect = () => {
   const navigate = useNavigate();
 
-  const redirectTo = (href: string, options: NavigateOptions) =>
-    navigate(href, options);
+  const redirectTo = (href: string, options?: NavigateOptions) => navigate(href, options);
 
   return {
     redirectTo,
