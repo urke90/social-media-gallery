6c61447c04a907ba14d3dd8def128fa1c1629115
urke90 <urosbijelic90@gmail.com>
2024-02-01 10:46:32 +0100

    added spinner component

---
+++ frontend/src/components/common/Spinner.tsx
@@ -0,0 +1,25 @@
+// react-loader-spinner
+import { ThreeCircles, type _Props4 } from 'react-loader-spinner';
+
+// ----------------------------------------------------------------
+
+interface ISpinnerProps extends _Props4 {
+  message?: string;
+}
+
+const Spinner: React.FC<ISpinnerProps> = ({
+  height = 50,
+  width = 200,
+  color = '#00BFFF',
+  message,
+  ...rest
+}) => {
+  return (
+    <div className="flex flex-col justify-center item-center w-full, h-full">
+      <ThreeCircles color={color} height={height} width={width} wrapperClass="m-5" {...rest} />
+      {message && <div className="text-lg text-center px-2">{message}</div>}
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ frontend/src/components/layout/index.ts
@@ -0,0 +1,1 @@
+export { default as MasonryLayout } from './MasonryLayout';
